9f722b5d469923537498a36fb7db83ccafbb7df8
strongcookdas <goodjav141592@gmail.com>
2022-11-30 13:39:48 +0900

    build : study java

---
+++ src/main/java/com/example/study_java2/StudyJava2Application.java
@@ -0,0 +1,13 @@
+package com.example.study_java2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJava2Application.class, args);
+	}
+
+}
